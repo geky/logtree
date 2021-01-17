a87df990ce87ed683fb654d842cf29030c976e5c
Christopher Haster <chaster@utexas.edu>
2021-01-16 23:13:40 -0600

    Things working?? And expiremental rotations!

--- logtree.py
+++ logtree.py
@@ -52,91 +52,56 @@ class LogTree:
             nkey = node.key
             for altkey, altoff in node.alts:
                 if altkey > lo and altkey < hi:
-                    if key < altkey and altkey < node.key:
+                    if key <= altkey and altkey <= node.key:
                         hi = altkey
                         nkey = altkey
                         noff = altoff
                         break
-                    elif key > altkey and altkey > node.key:
+                    elif key >= altkey and altkey >= node.key:
                         lo = altkey
                         nkey = altkey
                         noff = altoff
                         break
-                    elif key == altkey:
-                        break
-                    elif key < altkey:
+#                    elif key == altkey: # TODO wait this is messing with things
+#                        break
+                    elif key <= altkey:
                         hi = altkey
                         alts.append((altkey, altoff))
-                    elif key > altkey:
+                    elif key >= altkey:
                         lo = altkey
                         alts.append((altkey, altoff))
 
             # TODO need this?
-            if not alts or nkey != alts[-1][0]:
-                alts.append((nkey, off))
+            #if not alts or nkey != alts[-1][0]:
+            # TODO is there some room for optimizing redundant branches?
+            alts.append((nkey, off))
             off = noff
 
-#            # build new alt-pointers
-#            noff = -1
-#            nkey = self.nodes[off].key
-#            for altkey, altoff in node.alts:
-#                if key < altkey and altkey > node.key:
-#                    alts.append((altkey, off))
-#                    noff = altoff
-#                elif key > altkey and altkey < node.key:
-#                    alts.append((altkey, off))
-#                    noff = altoff
-#                else:
-#                    alts.append((altkey, altoff))
-#            else if :
-#                alts.append((node.key, off))
-                
-                    
+            # remove random suffix? TODO this good? TODO answer is no
+            if len(alts) > 1:
+                #alts = alts[:random.randrange(len(alts))+1]
+                #alts = alts[:1]
+                #alts = [(alts[0][0], max(x for _, x in alts))]
+                pass
 
-
-#                if altkey > lo and altkey < hi:
-#                    
-#
-#
-#
-#
-#                    alts.append((altkey, altoff))
-#
-#                    if key <= altkey and altkey < node.key:
-#                        hi = altkey
-#                        noff = altoff
-#                        break
-#                    elif key >= altkey and altkey > node.key:
-#                        lo = altkey
-#                        noff = altoff
-#                        break
-#                    elif key <= altkey:
-#                        hi = altkey
-#                        pass
-#                    elif key >= altkey:
-#                        lo = altkey
-#                        pass
-##
-#
-#                    if key <= altkey:
-#                        hi = altkey+1
-#                        if altkey < node.key:
-#                            alts.append((altkey+1, off))
-#                            noff = altoff
-#                            break
-#                        else:
-#                            alts.append((altkey, altoff))
-#                    elif key >= altkey:
-#                        lo = altkey-1
-#                        if altkey > node.key:
-#                            alts.append((altkey-1, off))
-#                            noff = altoff
-#                            break
-#                        else:
-#                            alts.append((altkey, altoff))
-#            else:
-#            if not alts:
-
+            # rotate shenanigans?
+            nalts = []
+            i = 0
+            while i < len(alts):
+                if i < len(alts)-1 and (
+                        key > alts[i+1][0] and alts[i+1][0] > alts[i+0][0] and
+                        alts[i+1][1] > alts[i+0][1]):
+                    nalts.append(alts[i+1])
+                    i += 2
+                elif i < len(alts)-1 and (
+                        key < alts[i+1][0] and alts[i+1][0] < alts[i+0][0] and
+                        alts[i+1][1] > alts[i+0][1]):
+                    nalts.append(alts[i+1])
+                    i += 2
+                else:
+                    nalts.append(alts[i])
+                    i += 1
+            alts = nalts
 
         # append
         self.nodes.append(LogTree.Node(key, value, alts=alts))
@@ -152,21 +117,26 @@ class LogTree:
             if node.key == key:
                 return node.value
 
-            if key < node.key and node.key < hi:
-                hi = node.key
-            elif key > node.key and node.key > lo:
-                lo = node.key
+#            if key == 5:
+#                print('5 => %s' % node)
+
+#            if key < node.key and node.key < hi:
+#                hi = node.key
+#            elif key > node.key and node.key > lo:
+#                lo = node.key
 
             # build new alt-pointers
             for altkey, altoff in node.alts:
                 if altkey > lo and altkey < hi:
-                    if key == altkey:
+#                    if key == altkey:
+#                        off = altoff
+#                        break
+                    if key <= altkey and altkey <= node.key:
+                        hi = altkey if altkey != key else hi # TODO hm
                         off = altoff
                         break
-                    elif key < altkey and altkey < node.key:
-                        off = altoff
-                        break
-                    elif key > altkey and altkey > node.key:
+                    elif key >= altkey and altkey >= node.key:
+                        lo = altkey if altkey != key else lo # TODO hm
                         off = altoff
                         break
             else:
